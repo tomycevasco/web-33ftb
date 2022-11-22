74d6b5dd6c36b2dab5859678dbe6167c8d539217
Tomas Cevasco <tomycevasco@live.com>
2022-11-22 00:04:56 -0300

    Primera homework terminada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,43 @@
 'use strict';
+//no uar parseint
+//no usar .tostring
+//de string a numerto
+function BinarioADecimal(string) {
+   let sum = 0
+   let arr = string.split('').reverse()
+   for (let i = 0; i < arr.length; i++){
+      sum += Math.pow(2, i) * arr[i];
+   }
+   return sum;
+} 
 
-function BinarioADecimal(num) {}
 
-function DecimalABinario(num) {}
 
+
+
+//de numero a string
+function DecimalABinario(num) {
+   let almacenador = []
+   while (num !== 0) {
+      let residuo = (num % 2)
+      num = Math.floor(num / 2);
+      almacenador.push(residuo)
+   }
+   return almacenador.reverse().join('')
+   
+
+   
+   
+      
+
+
+
+
+
+
+
+
+ }
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
