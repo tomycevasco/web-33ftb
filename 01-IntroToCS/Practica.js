ffde8db8b21a7ca954f7d4c0ce83cf71b1be1a9b
Tomas Cevasco <tomycevasco@live.com>
2022-11-28 00:00:39 -0300

    practica hasta object.create

--- 01-IntroToCS/Practica.js
+++ 01-IntroToCS/Practica.js
@@ -5,12 +5,7 @@ let pileta = {
         console.log('wowow')
     }
 }
-let { chica, grande} = pileta
-console.log(chica())
-console.log(grande)
-let arraysito = [1, 4, 8, 10, 150]
-let [chimichimi, four, six] = arraysito
-console.log(six)
+
 //-------------------------------------------------------------//
 //METODO FOR EACH
 array = [1, 2, 3, 4, 5]
@@ -18,6 +13,12 @@ array.forEach((a,c)=> {
     console.log(a,c)
     
 });
+let { chica, grande } = pileta
+console.log(chica())
+console.log(grande)
+let arraysito = [1, 4, 8, 10, 160]
+let [chimi, four, sixx] = arraysito
+console.log(sixx)
 function multi() {
     var a = 15
     var b = 18
@@ -26,6 +27,20 @@ function multi() {
 }
 console.log(multi())
 //------------------------------------------------------------------//
-
-console.log
+//OBJECT.CREATE// SIRVE PARA CREAR UN OBJETO CON OTRO YA PREDETERMINADO.
+const person = {
+    holaa: 'pupi',
+    puto: 'putaso',
+    saludo() {
+        return 'hola '+ this.holaa+ ' soy re '+ this.puto 
+    }
+}
+var hola = Object.create(person)
+console.log(person)
+hola.holaa  = 'pipito'
+hola.puto = 'pelotudito'
+console.log(hola.saludo())
+console.log(person.saludo())
+console.log(hola.__proto__)
+//---------------------------------------------------------------//
 
